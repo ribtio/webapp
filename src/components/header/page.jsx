d530195a0b1bfd28c670fc746051dfa2ab8291dd
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-05-07 07:44:19 -0300

    feat: header add

---
+++ src/components/header/page.jsx
@@ -0,0 +1,20 @@
+import { Image } from "@nextui-org/react";
+import Logo from "../../assets/logo-ribt.svg";
+export default function Header() {
+  return (
+    <div>
+      <div className="w-full h-36 flex">
+        <div className="w-full flex justify-center items-center px-12">
+          <Image
+            className=""
+            src={Logo}
+            width={130}
+            height={170}
+            alt="Logotipo"
+          />
+          <div className="w-full h-[2px] bg-black ml-8"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
