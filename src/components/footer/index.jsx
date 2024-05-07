db4e2c5aa104aabb9151e0c9c029cc72407ab3ff
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-05-07 08:48:16 -0300

    feat: add footer and fix img components

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="w-full h-14 bg-[#006FEE] flex justify-end items-center">
+      <span className="text-white mr-5">
+        © 2024 Ribt, Inc. All Rights Reserved.
+      </span>
+    </div>
+  );
+};
+export default Footer;
