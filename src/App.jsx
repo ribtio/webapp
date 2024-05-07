7207a8a69a6e644f78c25203402862fbcbf01487
gabrielpriegoo <gabrielvpriego@gmail.com>
2024-05-07 07:26:45 -0300

    setup initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Clickme</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
